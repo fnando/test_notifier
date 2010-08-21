37c5fb40ef53a451fd36ebb4d34c4011840d8001
Jefferson Girao <contato@jefferson.eti.br>
2010-08-21 15:22:49 -0300

    Minor fix on message, matching the right data to pending.

--- lib/test_notifier/runner/autotest.rb
+++ lib/test_notifier/runner/autotest.rb
@@ -3,13 +3,15 @@ require "test_notifier"
 Autotest.add_hook :ran_command do |at|
   begin
     content = at.results.to_s
+
     rspec_matches = content.match(/(\d+) examples?, (\d+) failures?(, (\d+) pendings?)?/)
     test_unit_matches = content.match(/(\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors/)
 
     if rspec_matches
-      _, examples, failures, pending = *rspec_matches
+      _, examples, failures, _, pending = *rspec_matches
       status = failures.to_i > 0 ? :fail : :success
-      message = "#{examples} examples, #{failures} failed, #{pending} pending"
+      message = "#{examples} examples, #{failures} failed"
+      message << ", #{pending} pending" if pending
       TestNotifier.notify(:status => status, :message => message) unless examples.to_i.zero?
     elsif test_unit_matches
       _, tests, assertions, failures, errors = *test_unit_matches
