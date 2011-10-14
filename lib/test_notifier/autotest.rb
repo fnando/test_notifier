a9129946e1e8312a42db47317156e0a02b166092
Nando Vieira <fnando.vieira@gmail.com>
2011-10-14 20:56:44 -0300

    Removed deprecated runners.

--- lib/test_notifier/autotest.rb
+++
@@ -1,7 +0,0 @@
-require "test_notifier/runner/autotest"
-
-warn <<-TXT
-[TestNotifier] Using `require "test_notifier/autotest"` is deprecated and
-will be removed in the future. Please update your `~/.autotest` file to use
-`require "test_notifier/runner/autotest"` instead.
-TXT
--- lib/test_notifier/test_unit.rb
+++
@@ -1,6 +0,0 @@
-require "test_notifier/runner/test_unit"
-
-warn <<-TXT
-[TestNotifier] Using `require "test_notifier/test_unit"` is deprecated and
-will be removed in the future. Please use `require "test_notifier/runner/test_unit"` instead.
-TXT
